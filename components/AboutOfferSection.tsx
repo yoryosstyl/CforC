27059c1615a86fd4a27ba415d76ab73cfb13f143
Yoryos Styl <51874611+yoryosstyl@users.noreply.github.com>
2025-11-09 11:14:56 +0200

    Change section background color from white to gray

--- components/AboutOfferSection.tsx
+++ components/AboutOfferSection.tsx
@@ -79,7 +79,7 @@ export default function AboutOfferSection() {
   }, [])
 
   return (
-    <section className="py-24 bg-white">
+    <section className="py-24 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div
