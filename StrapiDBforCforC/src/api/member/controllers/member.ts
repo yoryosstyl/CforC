65ea68d5e944324b976b4a154e3f88132de405b4
Nejigs <yoryosstyl@MacBookPro.router.wind>
2025-11-28 23:04:21 +0200

    Add custom Strapi controller to allow auth field updates

--- StrapiDBforCforC/src/api/member/controllers/member.ts
+++ StrapiDBforCforC/src/api/member/controllers/member.ts
@@ -4,4 +4,17 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::member.member');
+export default factories.createCoreController('api::member.member', ({ strapi }) => ({
+  // Override the default update to allow updating auth fields
+  async update(ctx) {
+    const { id } = ctx.params;
+    const { data } = ctx.request.body;
+
+    // Allow updating sensitive auth fields through the API
+    const entity = await strapi.entityService.update('api::member.member', id, {
+      data,
+    });
+
+    return { data: entity };
+  },
+}));
